3ee4b8f1bc4581a0bbdf038f8cb24421ab767b4c
articnine9 <articnine9@gmail.com>
2024-09-04 10:39:04 +0530

    deleted customer

--- frontend/src/Routes/appRoutes.js
+++ frontend/src/Routes/appRoutes.js
@@ -8,12 +8,7 @@ import TableOrderList from "../Admin/Components/TableOrdetList/TableOrderList";
 import Profile from "../Admin/Components/AdminProfile/Profile";
 import RecentBills from "../Admin/Components/Recent/RecentBills";
 import AdminOrder from "../Admin/Components/AdminOrder/AdminOrder";
-import Qrcode from "../Customer/Components/QrCode/qrCode";
-import Menu from "../Customer/Components/CustomerMenu/menu";
-import AddToCart from "../Customer/Components/AddToCart/addToCart";
-import OfflinePayment from "../Customer/Components/CustomerPayment/offlinePayment";
-import OrderStatus from "../Customer/Components/CustomerOrderStatus/orderStatus";
-import PhoneNumberLogin from "../Customer/Components/PhoneNumberLogin/phoneNumberLogin";
+
 import KitchenPage from '../Kitchen/Components/KitchenPage/kitchen';
 import KitchenStocks from '../Kitchen/Components/KitchenStocks/kitchenStocks';
 
@@ -29,12 +24,7 @@ const AppRoutes = () => (
       <Route path="/recent" element={<RecentBills />} />
       <Route path="/adminOrder" element={<AdminOrder />} />
 
-      <Route path="/qrCode" element={<Qrcode />} />
-      <Route path="/customerPage" element={<Menu />} />
-      <Route path="/addToCart" element={<AddToCart />} />
-      <Route path="/payment" element={<OfflinePayment />} />
-      <Route path="/orderStatus" element={<OrderStatus />} />
-      <Route path="/login" element={<PhoneNumberLogin />} />
+
 
       <Route path="/kitchenPage" element={<KitchenPage/>}/>
          <Route path="/kitchenStocks" element={<KitchenStocks/>}/> 
